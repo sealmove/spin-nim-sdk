1eae0d18a3cd816bc425b857b881f2d15848a46a
sealmove <sealmove@protonmail.com>
2022-09-08 01:31:45 +0300

    change util operator
    Signed-off-by: sealmove <sealmove@protonmail.com>

--- src/spin/http/utils.nim
+++ src/spin/http/utils.nim
@@ -1,13 +1,13 @@
 import private/types
 import std/[options, tables]
 
-proc `?`*(headers: HttpHeaders, key: string): Option[string] =
+proc `?&`*(headers: HttpHeaders, key: string): Option[string] =
   if headers.hasKey(key):
     some(headers[key])
   else:
     none(string)
 
-proc `?`*(params: seq[tuple[key, value: string]], key: string): Option[string] =
+proc `?&`*(params: seq[tuple[key, value: string]], key: string): Option[string] =
   for (k, v) in params:
     if k == key:
       return some(v)
